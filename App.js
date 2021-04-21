750157eec542cf859b5ec806ea57a70e548046fa
omerenma <godwin2341@gmail.com>
2021-04-21 12:09:55 +0100

    langing screen

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.containers}>
+      <Text style={styles.text}>Food App!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containers: {
+    justifyContent: "center",
+  },
+  text: {
+    alignItems: "center",
+    color: "red",
   },
 });
